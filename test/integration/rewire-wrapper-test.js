5a972d8686184b3a86308689a7cbe2b07edc4823
ryym <ryym.64@gmail.com>
2016-01-01 15:04:02 +0900

    test: Add integration tests of rewiring

--- test/integration/rewire-wrapper-test.js
+++ test/integration/rewire-wrapper-test.js
@@ -3,7 +3,7 @@ import rewire from '../../lib/babel-rewire-wrapper';
 import greeter from './module/greeter';
 
 /**
- * Do actual rewiring to test modules.
+ * Do actual rewiring.
  */
 describe('Integration: babel-rewire wrapper', () => {
   function assertGreet(expectedGreet) {
@@ -20,17 +20,46 @@ describe('Integration: babel-rewire wrapper', () => {
     assertGreet('Hello');
   });
 
-  it('injects mocks to registered modules');
-  it('resets dependencies of registered modules');
-
   context('run with sync function', () => {
-    it('injects mocks while running the action');
-    it('resets injected mocks after the action');
+    it('injects mocks while running the action', () => {
+      assertGreet('Hello');
+      rewire()
+        .use(greeter, { greet: () => 'Hi' })
+        .run(() => assertGreet('Hi'));
+    });
+
+    it('resets injected mocks after the action', () => {
+      assertGreet('Hello');
+      rewire()
+        .use(greeter, { greet: () => 'Hi' })
+        .run(() => {});
+      assertGreet('Hello');
+    });
   });
 
   context('run with async function', () => {
-    it('injects mocks when running the action');
-    it('resets injected mocks after the action');
-  });
+    it('injects mocks when running the action', done => {
+      assertGreet('Hello');
+      rewire()
+        .use(greeter, { greet: () => 'Hi' })
+        .run(reset => {
+          setTimeout(() => {
+            assertGreet('Hi');
+            reset();
+          }, 5);
+        })
+        .then(done, done);
+    });
 
+    it('resets injected mocks after the action', done => {
+      assertGreet('Hello');
+      rewire()
+        .use(greeter, { greet: () => 'Hi' })
+        .run(reset => {
+          setTimeout(reset, 5);
+        })
+        .then(() => assertGreet('Hello'))
+        .then(done, done);
+    });
+  });
 });
