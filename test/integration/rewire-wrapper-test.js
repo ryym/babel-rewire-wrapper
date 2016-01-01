c307c81bbcca1d7130214b43c75b231468d9116e
ryym <ryym.64@gmail.com>
2016-01-01 14:56:33 +0900

    test: Ensure rewire-plugin exists

--- test/integration/rewire-wrapper-test.js
+++ test/integration/rewire-wrapper-test.js
@@ -1,8 +1,24 @@
+import assert from 'power-assert';
+import rewire from '../../lib/babel-rewire-wrapper';
+import greeter from './module/greeter';
 
 /**
  * Do actual rewiring to test modules.
  */
 describe('Integration: babel-rewire wrapper', () => {
+  function assertGreet(expectedGreet) {
+    assert.equal(greeter.greet(), expectedGreet);
+  }
+
+  it('has babel-plugin-rewire', () => {
+    assertGreet('Hello');
+    greeter.__Rewire__('greet', () => 'Hi');
+
+    assertGreet('Hi');
+
+    greeter.__ResetDependency__('greet');
+    assertGreet('Hello');
+  });
 
   it('injects mocks to registered modules');
   it('resets dependencies of registered modules');
